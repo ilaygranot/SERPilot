9a522af743a81d7d941124a86af94b8960275db4
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2023-05-02 01:34:23 +0300

    remove definition from original main

--- serpilot.py
+++ serpilot.py
@@ -1,3 +1,5 @@
+# main.py
+
 from io import BytesIO
 import tempfile
 import streamlit as st
@@ -20,120 +22,95 @@ from app import (
 )
 from expanders import expanders
 
-def generate_text(api_key, model, prompt, max_tokens, n, stop, temperature, top_p):
-    openai.api_key = api_key
 
-    response = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=max_tokens,
-        n=n,
-        stop=stop,
-        temperature=temperature,
-        top_p=top_p,
-    )
+def main():
+    st.set_page_config(page_title="AI Content Factory", page_icon=None, layout='centered',
+                       initial_sidebar_state='expanded')
+    st.title("AI Content Factory")
+    st.write("A powerful AI-driven content generation tool for creating high-quality articles at scale.")
+    render_expanders(expanders)
 
-    return response
-
-def article_app():
-    st.title("Article Generation")
-    st.subheader("Generate high-quality, SEO-friendly content using GPT-4")
-
-    # UI Elements for Article Generation
     with st.sidebar:
-        st.header("Article Generation Parameters")
-        openai_api_key = st.text_input("OpenAI API Key", value="", help="Enter your OpenAI API Key.", type="password")
-        gpt_model = st.selectbox("GPT Model", ["gpt-3.5-turbo", "gpt-4"], help="Select the GPT model you want to use for generating content.")
-        marketing_term = st.text_input("Marketing Term", value="", help="Enter the marketing term you want to generate an article about.")
-        generate_button = st.button("Generate Article")
+        st.header("Settings")
 
-    if generate_button:
-        if not openai_api_key:
-            st.error("Please provide an OpenAI API Key.")
-        elif not marketing_term:
-            st.error("Please provide a marketing term.")
-        else:
-            # Add your code here to generate the article using the OpenAI API
-            openai.api_key = openai_api_key
-            generated_article = generate_text(
-                    openai_api_key,
-                    model=gpt_model,
-                    prompt=prompts["article_prompt"].format(
-                    term, outline, prompts["related_links_prompt"].format("\n".join(related_links))
-                ),
-                max_tokens=1024,
-                n=1,
-                stop=None,
-                temperature=0.8,
-                top_p=1,
-            )
+        api_key = st.text_input("API Key:", value="", type="password")
+        uploaded_file = st.file_uploader("Upload a CSV file:", type=["csv"])
 
-        generated_article = generated_article.choices[0].text.strip()
+        domain = st.text_input("URL Path:", value="")
 
-        st.header("Generated Article")
-        st.write(generated_article)
+        # Model and settings
+        model = st.selectbox("Model:", ["gpt-3.5-turbo", "gpt-4"])
+        temperature = st.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.7, step=0.1)
+        max_tokens = st.slider("Max Tokens:", min_value=1, max_value=8000, value=2048, step=1)
+        presence_penalty = st.slider("Presence Penalty:", min_value=-2.0, max_value=2.0, value=0.2, step=0.1)
+        frequency_penalty = st.slider("Frequency Penalty:", min_value=-2.0, max_value=2.0, value=0.2, step=0.1)
+
+        # Add the input field for the section start column
+        section_start_col = st.number_input("Section Start Column (default is 7)", min_value=1, value=7, step=1)
+
+    if st.button("Generate Articles"):
+        if not api_key or not uploaded_file:
+            st.error("Please provide all required inputs (API Key, Domain Name, and CSV File).")
+            return
+
+        df = pd.read_csv(uploaded_file)
+        df.columns = map(str.lower, df.columns)  # Convert all column names to lowercase
+        df["url path"] = df["keyword / h1"].apply(create_url_path)
+        df["full path"] = df["url path"].apply(lambda x: create_full_path(domain, x))
+
+        topics = df["topic"].tolist()
+        h1_keywords = df["keyword / h1"].tolist()
+        sections = df.iloc[:, 7:].values.tolist()
+
+        progress_text = "Generating articles. Please wait..."
+        my_bar = st.progress(0)
+        total_items = len(topics) * 2
+        # definitions = []
+        articles = []
+        for idx, (topic, sec) in enumerate(zip(topics, sections)):
+            related_links = generate_related_links(df, topic)
+
+            # definition = generate_article(api_key, topic, sec, related_links, model, temperature, presence_penalty,
+                                          # frequency_penalty, max_tokens)
+            # definitions.append(definition)
+            time.sleep(7)
+            my_bar.progress((((idx + 1) * 2 - 1) / total_items * 100) / 100)
+
+            article = generate_article(api_key, topic, sec, related_links, model, temperature, presence_penalty,
+                                       frequency_penalty, max_tokens)
+            articles.append(article)
+            time.sleep(7)
+            my_bar.progress((((idx + 1) * 2) / total_items * 100) / 100)
+
+        # Create a temporary directory to store the generated DOCX files
+        timestamp = dt.now().strftime('%Y-%m-%d_%H-%M-%S')
+        with tempfile.TemporaryDirectory() as temp_dir:
+            output_dir = os.path.join(temp_dir, f"article_batch_{timestamp}")
+            os.makedirs(output_dir)
+
+            for idx, (topic, h1_keyword, article) in enumerate(
+                    zip(topics, h1_keywords, articles)):
+                docx_filename = f"{output_dir}/{topic.replace(' ', '_')}_article.docx"
+                save_article_as_docx(docx_filename, h1_keyword, article)
+
+            # Save the updated DataFrame to a new CSV file
+            output_file = f"{output_dir}/article_batch_{timestamp}.csv"
+            df.to_csv(output_file, index=False)
+
+            with zipfile.ZipFile(f"{temp_dir}/article_batch_{timestamp}.zip", "w") as zipf:
+                for folder, _, filenames in os.walk(output_dir):
+                    for filename in filenames:
+                        file_path = os.path.join(folder, filename)
+                        zipf.write(file_path, os.path.basename(file_path))
+
+            st.success(f"Generated articles and definitions added to 'article_batch_{timestamp}.zip'.")
+
+            with open(f"{temp_dir}/article_batch_{timestamp}.zip", "rb") as f:
+                bytes = f.read()
+                b = BytesIO(bytes)
+                st.download_button("Download Generated Articles", b, f"article_batch_{timestamp}.zip",
+                                   "application/zip")
 
 
-        # Display the generated article
-        st.header("Generated Article")
-        st.write("The generated article will be displayed here.")
-
-def serp_app():
-    st.title("SERP Outline")
-    st.subheader("Extract search results using SpaceSerp API and generate content outlines")
-
-    # UI Elements for SERP Outline
-    with st.sidebar:
-        st.header("Parameters")
-        api_key = st.text_input("API Key", value="", help="Enter your SpaceSerp API Key.", type="password")
-        query_text = st.text_area("Query", value="", help="Enter one or multiple keywords to search (one per line).")
-        search_queries = [q.strip() for q in query_text.split('\n') if q.strip()]
-        domain = st.selectbox("Domain", [x["domain"] for x in google_domains])
-        gl = st.selectbox("Country Code", [x["code"] for x in google_countries])
-        hl = st.selectbox("Language Code", [x["code"] for x in google_languages])
-        device = st.selectbox("Device", devices)
-        mobile_os = st.selectbox("Mobile OS", mobile_os) if device != "desktop" else None
-        page_size = st.number_input("Page Size", min_value=1, max_value=100, value=10, step=1)
-        page_number = st.number_input("Page Number", min_value=1, max_value=100, value=1, step=1)
-        result_blocks = st.multiselect("Result Blocks", result_block_options, help="Select the types of results you want to include.")
-
-        # GPT Model Parameters
-        openai_api_key = st.text_input("OpenAI API Key", value="", help="Enter your OpenAI API Key.", type="password")
-        gpt_model = st.selectbox("GPT Model", ["gpt-3.5-turbo", "gpt-4"], help="Select the GPT model you want to use for generating outlines.")
-
-    if st.button("Fetch Search Results"):
-        if not api_key:
-            st.error("Please provide an API key.")
-        elif not query_text:
-            st.error("Please provide a query.")
-        elif not openai_api_key:
-            st.error("Please provide an OpenAI API Key.")
-        else:
-            # Fetch Search Results
-            search_results = get_search_results(api_key, search_queries, domain, gl, hl, device, mobile_os, page_size, page_number, result_blocks)
-            if search_results:
-                # Generate Outlines
-                outlines = []
-                for result in search_results:
-                    outline = generate_outline(openai_api_key, gpt_model, result['title'], result['snippet'], result['url'])
-                    outlines.append(outline)
-
-                # Display Outlines
-                st.header("Search Results & Outlines")
-                for i, (result, outline) in enumerate(zip(search_results, outlines)):
-                    st.subheader(f"{i + 1}. {result['title']}")
-                    st.write(f"URL: {result['url']}")
-                    st.write(f"Snippet: {result['snippet']}")
-                    st.write("Outline:")
-                    st.write(outline)
-            else:
-                st.warning("No search results found for the given query.")
-
-# Main App
-st.set_page_config(page_title="GPT-4 Powered Content Generation", layout="wide")
-st.sidebar.title("Choose a Mode")
-mode = st.sidebar.radio("", ["Article Generation", "SERP Outline"])
-if mode == "Article Generation":
-    article_app()
-else:
-    serp_app()
+if __name__ == "__main__":
+    main()
