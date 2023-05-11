6fa30c8d5d420d65088b14264b90ed2f0a04aa8e
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2023-05-11 23:34:52 +0300

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -3,15 +3,14 @@
 prompts = {
     "system_message": (
         """
-You are an AI language model. Your task is to follow the provided outline and ensure that the content is well-structured, SEO-friendly, and addresses the key points in each section.
-Make sure to use clear, concise language and provide practical advice, examples, and tips where applicable.
-"""),
+You are an AI language model. You have been given a set of keywords and an outline to write an article. Your task is to write an SEO-friendly article that is well-structured and addresses the key points outlined in each section. The article should use clear and concise language, providing practical advice, examples, and tips where relevant. The output should be in semantic HTML format."""
+    ),
     "article_prompt": (
         """
-Please write an informative and SEO-optimized article about the topic '{}' following the given outline:\n\n{}\n
-Please provide the output in semantic HTML format (there is no need for the H1)."""),
+Given the topic '{}' and the following outline:\n\n{}, you are to write an informative and SEO-optimized article. The article should follow the provided outline closely, addressing each point with appropriate detail. Make sure to infuse the given keywords naturally throughout the text for SEO optimization. The output should be in semantic HTML format, which includes appropriate tags such as <h1>, <p>, etc."""
+    ),
     "related_links_prompt": (
         """
-In your HTML output, incorporate the following related links into the article text by using relevant anchor text when applicable. Here are the related links to incorporate:\n\n{}.
-""")
+You have been provided with the following related links:\n\n{}. Your task is to incorporate these links naturally into the text of the article. Use relevant anchor text that aligns with the content of the link. Make sure to insert each link in the correct HTML format, using the <a href=""></a> tag, within the body of the article where it is most relevant."""
+    )
 }
